f884ba0190786a5258aef7a940cf788e5dbaf183
tech.nehaverma@gmail.com <tech.nehaverma@gmail.com>
2026-08-29 23:07:44 +0530

    zipkin implemented in order service

--- order/src/main/java/com/ecommerce/order/configuration/OrderConfiguration.java
+++ order/src/main/java/com/ecommerce/order/configuration/OrderConfiguration.java
@@ -1,8 +1,11 @@
 package com.ecommerce.order.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -10,15 +13,44 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 import com.ecommerce.order.clients.ProductServiceRestClient;
 import com.ecommerce.order.clients.UserServiceRestClient;
 
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.propagation.Propagator;
+
 @Configuration
 public class OrderConfiguration {
 	
-	@Bean
+	@Autowired(required = false)
+	private ObservationRegistry observationRegistry;
 	
+	@Autowired(required = false)
+	private Tracer tracer;
+	
+	@Autowired(required = false)
+	private Propagator propagator;
+	
+	@Bean
+	@LoadBalanced
 	public RestClient.Builder restClient() {
-		return RestClient.builder();
+		RestClient.Builder builder = RestClient.builder();
+		
+		if(observationRegistry!=null) {
+			builder.requestInterceptor(createTracingInterceptor());
+		}
+		return builder;
 	}
 	
+	private ClientHttpRequestInterceptor createTracingInterceptor() {
+		
+		return ((req,body,execution) -> {
+			if(tracer!=null && propagator!=null && tracer.currentSpan()!=null) {
+				propagator.inject(tracer.currentTraceContext().context(),
+						req.getHeaders(), (carrier,key,value)-> carrier.add(key, value));
+			}
+			return execution.execute(req, body);	
+		});
+	}
+
 	@Bean
 	@Lazy
 	public ProductServiceRestClient restproductClientInterface(RestClient.Builder builder) {
